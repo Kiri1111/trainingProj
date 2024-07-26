55039cc0e05751e2a7e2b3d5d539fc28fbce3bca
KirillMedvedev <92_medved@mail.ru>
2024-07-26 10:24:52 +0300

    fix: styles all app

--- src/MenuButtons.style.tsx
+++ src/MenuButtons.style.tsx
@@ -2,8 +2,9 @@ import Button from "@mui/material/Button/Button"
 import { styled } from "@mui/material/styles"
 
 export const MenuButton = styled(Button)({
-  minWidth: "110px",
+  minWidth: "90px",
   borderRadius: "3px",
   margin: "0 10px",
   color: "white",
+  backgroundColor: "rgba(9, 8, 8, 0.214)",
 })
