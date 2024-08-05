331206ccc126e7ae603a8762ddaba9938fafd9a7
Kiri1111 <92_medved@mail.ru>
2024-08-05 16:42:13 +0300

    feat: create store with redux

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,36 +1,35 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from "react"
-import s from "./Todolist.module.css"
-import Button from "@mui/material/Button"
-import SendIcon from "@mui/icons-material/Send"
-import TextField from "@mui/material/TextField"
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
 
 type AddItemFormProps = {
-  addItemCallBack: (newTitle: string) => void
-}
+  addItemCallBack: (newTitle: string) => void;
+};
 export const AddItemForm = ({ addItemCallBack }: AddItemFormProps) => {
-  const [newItemTitle, setNewItemTitle] = useState("")
-  const [error, setError] = useState<null | string>(null)
+  const [newItemTitle, setNewItemTitle] = useState("");
+  const [error, setError] = useState<null | string>(null);
 
   const onKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && newItemTitle.trim() !== "") {
-      addItemCallBack(newItemTitle.trim())
-      setNewItemTitle("")
+      addItemCallBack(newItemTitle.trim());
+      setNewItemTitle("");
     }
     if (e.key === "Enter" && newItemTitle.trim() === "")
-      setError("Title is required")
-  }
+      setError("Title is required");
+  };
   const addTaskHandler = () => {
     if (newItemTitle.trim() !== "") {
-      addItemCallBack(newItemTitle.trim())
+      addItemCallBack(newItemTitle.trim());
     } else {
-      setError("Title is required")
+      setError("Title is required");
     }
-    setNewItemTitle("")
-  }
+    setNewItemTitle("");
+  };
   const setNewTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setError(null)
-    setNewItemTitle(e.currentTarget.value)
-  }
+    setError(null);
+    setNewItemTitle(e.currentTarget.value);
+  };
 
   return (
     <div>
@@ -41,20 +40,21 @@ export const AddItemForm = ({ addItemCallBack }: AddItemFormProps) => {
           onChange={setNewTaskTitleHandler}
           error={!!error}
           helperText={error}
-          size='small'
-          label='Enter text'
-          variant='outlined'
+          size="small"
+          label="Enter text"
+          variant="outlined"
         />
 
         <Button
           sx={{ ml: "10px" }}
-          color='inherit'
+          color="inherit"
           variant={"contained"}
           endIcon={<SendIcon />}
-          onClick={addTaskHandler}>
+          onClick={addTaskHandler}
+        >
           ADD
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/state/store.ts
@@ -0,0 +1,15 @@
+import { combineReducers, createStore } from "redux";
+import { tasksReducer } from "../model/tasksReducer";
+import { todolistReducer } from "../model/todolistReducer";
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todolist: todolistReducer,
+});
+
+export const store = createStore(rootReducer);
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+//@ts-ignore
+window.store = store;
