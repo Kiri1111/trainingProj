80930fc2fba841a60ebb40ee12c9be649f9a9c33
Kiri1111 <92_medved@mail.ru>
2024-08-10 07:10:24 +0300

    feat: types for responses todolistApi

--- src/api/todolistApi.ts
+++ src/api/todolistApi.ts
@@ -10,15 +10,34 @@ const instance = axios.create({
 
 export const todolistApi = {
   getTodolists() {
-    return instance.get('todo-lists')
+    return instance.get<TodolistType[]>('todo-lists')
   },
   deleteTodolist(idTodolist: string) {
-    return instance.delete(`todo-lists/${idTodolist}`)
+    return instance.delete<ResponseType>(`todo-lists/${idTodolist}`)
   },
   updateTodolist(idTodolist: string, title: string) {
-    return instance.put(`todo-lists/${idTodolist}`, { title })
+    return instance.put<ResponseType>(`todo-lists/${idTodolist}`, { title })
   },
   createTodolist(title: string) {
-    return instance.post('todo-lists', { title })
+    return instance.post<ResponseType<{ item: TodolistType }>>('todo-lists', { title })
   },
+}
+
+type TodolistType = {
+  id: string
+  title: string
+  addedDate: Date
+  order: number
+}
+
+type ResponseType<T = {}> = {
+  resultCode: number
+  messages: string[]
+  fieldsErrors: FieldErrorType[]
+  data: T
+}
+
+type FieldErrorType = {
+  error: string
+  field: string
 }
