3748fe2304da558fe99068baaf6db4270ca0f750
Kiri1111 <92_medved@mail.ru>
2024-08-22 11:21:45 +0300

    feat: login component

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals'
 import { App } from './App'
 import { Provider } from 'react-redux'
 import { store } from './state/store'
+import { Login } from './features/login/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
